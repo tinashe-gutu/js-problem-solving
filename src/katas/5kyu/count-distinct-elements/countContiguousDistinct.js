a0f1110db5ba8ceadbe4d9422bec7bf2db6622ad
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-30 08:43:57 +0000

    format file

--- src/katas/5kyu/count-distinct-elements/countContiguousDistinct.js
+++ src/katas/5kyu/count-distinct-elements/countContiguousDistinct.js
@@ -5,49 +5,48 @@
 */
 
 /** return array of distinct numbers within windows with k length
- * 
- * @param {number} k 
- * @param {number} arr 
+ *
+ * @param {number} k
+ * @param {number} arr
  * @returns {number[]}
  */
 
 function countContiguousDistinct(arr, k) {
-        let kArray = arr.slice(0, k)
-      let distinctCount = new Set(kArray).size
-      let resualtArr = [distinctCount]
-      for(let i =0; i<arr.length-k; i++) {
-          let leaving = kArray.shift();
-          let entering = arr[i + k];
-          if(leaving != entering && (isInArray(kArray, leaving))){
-              if(isInArray(kArray, entering)){
-              }else{
-                  distinctCount++
-              }
-          }else if(leaving != entering && (!isInArray(kArray, leaving))){
-              if(isInArray(kArray, entering)){
-                  distinctCount--
-              }
-              }
-              kArray.push(entering)
-              resualtArr.push(distinctCount)
-          }
-          return resualtArr;
-  }
-
-
-  /**Checks if number is in a given array
-   * 
-   * @param {number[]} arr - subarray of original array
-   * @param {number} item  - target number
-   * @returns {boolean}
-   */
-  function isInArray(arr, item){
-    for(num of arr){
-     if(num===item){
-         return true;
-     }
+  let kArray = arr.slice(0, k);
+  let distinctCount = new Set(kArray).size;
+  let resualtArr = [distinctCount];
+  for (let i = 0; i < arr.length - k; i++) {
+    let leaving = kArray.shift();
+    let entering = arr[i + k];
+    if (leaving != entering && isInArray(kArray, leaving)) {
+      if (isInArray(kArray, entering)) {
+      } else {
+        distinctCount++;
+      }
+    } else if (leaving != entering && !isInArray(kArray, leaving)) {
+      if (isInArray(kArray, entering)) {
+        distinctCount--;
+      }
     }
-    return false;
- }
+    kArray.push(entering);
+    resualtArr.push(distinctCount);
+  }
+  return resualtArr;
+}
 
-  module.exports = countContiguousDistinct;
+/**Checks if number is in a given array
+ *
+ * @param {number[]} arr - subarray of original array
+ * @param {number} item  - target number
+ * @returns {boolean}
+ */
+function isInArray(arr, item) {
+  for (num of arr) {
+    if (num === item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = countContiguousDistinct;
